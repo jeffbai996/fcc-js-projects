0823ff8ca8ae3cfa671e9b86990d1070da997502
jeffbai996 <j.bai@wustl.edu>
2023-03-28 15:08:05 -0700

    Add completed code for Palindrome Checker

--- palindrome_checker.js
+++ palindrome_checker.js
@@ -10,6 +10,14 @@
 // We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.
 
 function palindrome(str) {
+  // set string to lowercase and remove all non-alphanumeric characters
+  var str1 = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  // reverse string and compare to original
+  var str2 = str1.split("").reverse().join("");
+  if (str1 !== str2) {
+    return false;
+  }
+  // return true if strings are equal
   return true;
 }
 
