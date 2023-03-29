65b5cf62e478f38bc5157c9fbf1e15f022ee5246
jeffbai996 <j.bai@wustl.edu>
2023-03-29 14:41:45 -0700

    Add complete code for Cash Register

--- cash_register.js
+++ cash_register.js
@@ -36,8 +36,55 @@
 // ]
 
 function checkCashRegister(price, cash, cid) {
-  let change;
-  return change;
+  // Currency units
+  const currencyValues = [
+    ["PENNY", 0.01],
+    ["NICKEL", 0.05],
+    ["DIME", 0.1],
+    ["QUARTER", 0.25],
+    ["ONE", 1],
+    ["FIVE", 5],
+    ["TEN", 10],
+    ["TWENTY", 20],
+    ["ONE HUNDRED", 100]
+  ];
+
+  // Calculate change
+  let changeDue = cash - price;
+  let change = [];
+  let totalCid = cid.reduce((acc, curr) => acc + curr[1], 0);
+
+  // Check if cid is less than change due
+  if (totalCid < changeDue) {
+    return {status: 'INSUFFICIENT_FUNDS', change: [] };
+  } else if (totalCid === changeDue) {
+    return {status: 'CLOSED', change: cid};
+  } else {
+    // Loop through currency array
+    for (let i = currencyValues.length - 1; i >= 0; i--) {
+      let currencyName = currencyValues[i][0];
+      let currencyValue = currencyValues[i][1];
+      let currencyAmount = 0;
+      // While changeDue is greater than currencyValue and cid is greater than 0
+      while (changeDue >= currencyValue && cid[i][1] > 0) {
+        changeDue -= currencyValue;
+        cid[i][1] -= currencyValue;
+        currencyAmount += currencyValue;
+
+        // Round to two decimal places to avoid floating point issues
+        changeDue = Math.round(changeDue * 100) / 100;
+      }
+      // Add currency to change array
+      if (currencyAmount > 0) {
+        change.push([currencyName, currencyAmount]);
+      }
+    }
+    // Check if changeDue is greater than 0
+    if (changeDue > 0) {
+      return {status: 'INSUFFICIENT_FUNDS', change: []};
+    }
+    return {status: 'OPEN', change: change};
+  }
 }
 
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
